71a37d9b26c250960d817a0d5c43f9a850a2bc7c
Eli <cyronix787@gmail.com>
2020-08-06 10:40:20 +0000

    unmount component after test

--- src/tests/__tests__/Carousel.test.js
+++ src/tests/__tests__/Carousel.test.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {shallow} from '../setup/enzyme.setup'
+import {shallow, mount} from '../setup/enzyme.setup'
 import Carousel from '../../components/Carousel'
 import Slide from '../../components/Slide'
 import FallbackSlide from '../../components/FallbackSlide'
 
 
 describe('Carousel component', () => {
-    let component, div, wrapper
+    let component, wrapper
 
-    test('renders without crashing', () => {
+    test('renders text "No slides"', () => {
         component = <Carousel name='test' />
-        const div = document.createElement('div')
-        ReactDOM.render(component, div)
-        ReactDOM.unmountComponentAtNode(div)
+        wrapper = mount(component)
+        expect(wrapper.contains('No slides')).toBe(true)
+        wrapper.unmount(component)
     })
 
     test('renders 3 Slide components', () => {
@@ -26,6 +26,7 @@ describe('Carousel component', () => {
         )
         wrapper = shallow(component)
         expect(wrapper.find(Slide)).toHaveLength(3)
+        wrapper.unmount(component)
     })
 
     test('renders 2 Slide components, 1 FallbackSlide component', () => {
@@ -39,5 +40,6 @@ describe('Carousel component', () => {
         wrapper = shallow(component)
         expect(wrapper.find(Slide)).toHaveLength(2)
         expect(wrapper.containsMatchingElement(FallbackSlide)).toBeDefined()
+        wrapper.unmount(component)
     })
 })
