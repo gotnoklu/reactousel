8edf161fa123dd9cbbdfe8fb68ca53de7e1b7fda
Eli <cyronix787@gmail.com>
2020-08-06 10:50:17 +0000

    writes test for indicators box component

--- src/tests/__tests__/IndicatorsBox.test.js
+++ src/tests/__tests__/IndicatorsBox.test.js
@@ -1,18 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {shallow} from '../setup/enzyme.setup'
+import {mount} from '../setup/enzyme.setup'
 import IndicatorsBox from '../../components/IndicatorsBox'
 import {withCarouselTheme} from '../../styles'
 
 const IndicatorsWithTheme = withCarouselTheme(IndicatorsBox)
 
-describe('Indicators component', () => {
-    let component, div, wrapper
+describe('IndicatorsBox component', () => {
+    let component, wrapper
 
-    test('renders without crashing', () => {
-        component = <IndicatorsWithTheme id='test' indicators={1} handleIndicatorClick={() => console.log('click')} />
-        const div = document.createElement('div')
-        ReactDOM.render(component, div)
-        ReactDOM.unmountComponentAtNode(div)
+    component = <IndicatorsWithTheme id='test' indicators={1} handleIndicatorClick={() => true} />
+
+    test('returns true when handleIndicatorClick prop is called', () => {
+        wrapper = mount(component)
+        expect(wrapper.invoke('handleIndicatorClick')()).toBe(true)
+        wrapper.unmount(component)
+    })
+
+    test('returns one indicator', () => {
+        wrapper = mount(component)
+        expect(wrapper.find('i')).toHaveLength(1)
+        wrapper.unmount(component)
     })
 })
